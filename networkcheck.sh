840cfc0005573c63adca90f1af1a197593599a77
Sebastian Rosario <sebastian6417@gmail.com>
2020-10-17 19:29:06 -0400

    Add files via upload

---
+++ networkcheck.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# This script checks default Linux networking settings like, IP, Mac Addresses and internet connectivity. 
+# This script also logs fail2ban ban/unban actions, open ports, and dns info at var/log/networkcheck.log
+
+# Variable that holds the log directory, defaults to /var/log
+logDirectory="/var/log/networkcheck.log"
+
+# Matches any IP address that is given in the output of the command 'ip a'
+# It is stored in the ipa variable and then called later for formatting
+ipa=$(ip a | awk 'match($0,/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}/,x) {print x[0]}')
+
+# Matches any mac addresses in the output of ip a and stores it in the macaddr variable
+macaddr=$(ip a | awk 'match($0,/\w\w:\w\w:\w\w\:\w\w:\w\w\:\w\w/,x) {print x[0]}')
+
+# Gets ban/unban actions as well as IP addresses asociated with it using grep and regex
+fail=$(cat /var/log/fail2ban.log | grep -E "[Bb]an [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+
+
+# The commands that are used to retrieve the network info are stored in the function called main_commands()
+main_commands() {
+	# Prints the date and sends the output to the log
+	printf "\n=====%s=====\n" "`date +%m/%d/%Y' '%T`" | tee -a $logDirectory
+
+	# Prints the IP addresses that were stored in the $ipa variable 
+	printf "IP ADDRESSES:\n%s\n" "$ipa"
+
+	# Prints mac addresses from the output of 'ip a'
+	printf "\nMAC ADDRESSES:\n%s\n" "$macaddr"
+
+	# Prints fail2ban actions and ip addresses to the terminal and also appends it to the log file
+	echo -e "\nFAIL2BAN LOG ACTIONS:" | tee -a $logDirectory
+	echo -e "$fail\n" | tee -a $logDirectory
+
+	# Conditional that checks for positive output from the ping command
+	echo -e "\nPinging 8.8.8.8 to test internet connectivity..."
+	if ping -w 2 -c 2 8.8.8.8 | grep -E "[1-9] received";
+		then
+			echo -e "\nINTERNET CONNECTION: TRUE"
+		else
+			echo -e "\nINTERNET CONNECTION: FALSE"
+	fi
+
+	# Uses ss to check system for open ports, prints to log and prints to terminal 
+	echo -e "\nOPEN PORTS:" | tee -a $logDirectory
+	ss -tupl | grep LISTEN | tee -a $logDirectory
+
+	# Uses dig to check system for DNS info such as normal servers and root servers, prints to terminal and log 
+	echo -e "\nDNS INFO:" | tee -a $logDirectory
+	dig | tee -a $logDirectory
+
+}
+
+
+# This set of if statements checks if the user has passed the script a directory for the log to be created in 
+# or if it has been run in root in which case the default directory can be used
+if [ "$#" -ne 1 ] && [ "$EUID" -gt 0 ]
+	then
+		echo "Please specify a log file in command line or run as root"
+elif [ "$#" -ne 1 ] && [ "$EUID" -eq 0 ]
+	then
+		echo "Saving in /var/log/networkcheck.log"
+		main_commands
+elif [ "$#" -ne 0 ]
+	then
+		logDirectory=$1
+		echo "Saving in $1"
+		main_commands
+fi
