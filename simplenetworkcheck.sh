f0ff5b904c47345e1c7455411c9b3f3166fd463e
Sebastian Rosario <sebastian6417@gmail.com>
2020-10-17 20:30:42 -0400

    Update simplenetworkcheck.sh

--- simplenetworkcheck.sh
+++ simplenetworkcheck.sh
@@ -3,16 +3,15 @@
 # Variable that holds the log directory, defaults to /var/log
 logDirectory="/var/log/simplenetworkcheck.log"
 
+# Variable that holds the fail2ban log directory
+f2bLog="/var/log/fail2ban.log"
+
 # Matches any IP address that is given in the output of the command 'ip a'
 # It is stored in the ipa variable and then called later for formatting
 ipa=$(ip a | awk 'match($0,/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}/,x) {print x[0]}')
 
 # Matches any mac addresses in the output of ip a and stores it in the macaddr variable
 macaddr=$(ip a | awk 'match($0,/\w\w:\w\w:\w\w\:\w\w:\w\w\:\w\w/,x) {print x[0]}')
-
-# Gets ban/unban actions as well as IP addresses asociated with it using grep and regex
-fail=$(cat /var/log/fail2ban.log | grep -E "[Bb]an [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
-
 
 # The commands that are used to retrieve the network info are stored in the function called main_commands()
 main_commands() {
@@ -25,10 +24,18 @@ main_commands() {
 	# Prints mac addresses from the output of 'ip a'
 	printf "\nMAC ADDRESSES:\n%s\n" "$macaddr"
 
-	# Prints fail2ban actions and ip addresses to the terminal and also appends it to the log file
-	echo -e "\nFAIL2BAN LOG ACTIONS:" | tee -a $logDirectory
-	echo -e "$fail\n" | tee -a $logDirectory
-
+	# Checks to see if the fail2ban logs are in the right place, reads from log if it is, continues if it is not
+	if [ -f "$f2bLog" ];
+		then
+			# Gets ban/unban actions as well as IP addresses asociated with it using grep and regex
+			fail=$(cat /var/log/fail2ban.log | grep -E "[Bb]an [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+			# Prints fail2ban actions and ip addresses to the terminal and also appends it to the log file
+			echo -e "\nFAIL2BAN LOG ACTIONS:" | tee -a $logDirectory
+			echo -e "$fail\n" | tee -a $logDirectory
+		else
+			echo -e "\nFAIL2BAN LOGS NOT FOUND\n"
+	fi
+	
 	# Conditional that checks for positive output from the ping command
 	echo -e "\nPinging 8.8.8.8 to test internet connectivity..."
 	if ping -w 2 -c 2 8.8.8.8 | grep -E "[1-9] received";
